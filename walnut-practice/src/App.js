7b4dffaec9995a62a30e50972c7bcddb46f10f44
Nathan Stephens <nathan.yukio.stephens@gmail.com>
2022-04-12 12:26:47 -0700

    created react app and put old materialize template into App.js file to render as React app

--- walnut-practice/src/App.js
+++ walnut-practice/src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main class="container">
+
+        <section class="col">
+          <div class="row">
+            <h1>BE PREPARED</h1>
+          </div>
+          <div class="row">
+
+            <div class="container" id="forecast-container">
+              <div class="col s3"></div>
+              <div class="col">day 1</div>
+              <div class="col">day 2</div>
+              <div class="col">day 3</div>
+              <div class="col">day 4</div>
+              <div class="col">day 5</div>
+              <div class="col s3"></div>
+            </div>
+
+          </div>
+          <div class="row">
+            <h1>I am Nathan Stephens</h1>
+          </div>
+        </section>
+      </main><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </>
+
   );
 }
 
